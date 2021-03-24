bb89ead3c5a1e60d0e9f8f8726a721740d262795
Guanjun Li <lgj@ubuntu-3.lgj>
2021-03-24 10:53:02 +0800

    Fix bug in BufferPool

--- src/cache/BufferPool.h
+++ src/cache/BufferPool.h
@@ -1,51 +1,51 @@
 ﻿#pragma once
+#include "../config/Configure.h"
 #include "../header.h"
+#include "../utils/SpinMutex.h"
 #include <stack>
 #include <unordered_map>
-#include "../utils/SpinMutex.h"
-#include "../config/Configure.h"
+#include <vector>
 
 namespace storage {
 using namespace std;
-class Buffer
-{
+class Buffer {
 public:
-  inline static Byte* CalcAddr(Byte* sAddr) {
+  inline static Byte *CalcAddr(Byte *sAddr) {
     return sAddr - ((uint64_t)sAddr & (Configure::GetCacheBlockSize() - 1));
   }
 
 public:
   Buffer(uint32_t eleSize);
   ~Buffer();
-  Byte* Apply();
-  void Release(Byte* bys);
-  void Apply(vector<Byte*>& vct);
-  void Release(vector<Byte*>& vct, bool bAll);
+  Byte *Apply();
+  void Release(Byte *bys);
+  void Apply(vector<Byte *> &vct);
+  void Release(vector<Byte *> &vct, bool bAll);
   void Init(uint32_t eleSize);
   inline bool IsEmpty() { return _stackFree.size() == 0; }
   inline bool IsFull() { return _stackFree.size() == _maxEle; }
-  inline Byte* GetBuf() { return _pBuf; }
+  inline Byte *GetBuf() { return _pBuf; }
 
 protected:
-  Byte* _pBuf;
+  Byte *_pBuf;
   uint32_t _eleSize;
   uint32_t _maxEle;
   stack<uint32_t> _stackFree;
 };
 
-class BufferPool
-{
+class BufferPool {
 public:
   BufferPool(uint32_t eleSize);
   ~BufferPool();
-  void Apply(vector<Byte*>& vct);
-  void Release(vector<Byte*>& vct, bool bAll);
-  Byte* Apply();
-  void Release(Byte* bys);
+  void Apply(vector<Byte *> &vct);
+  void Release(vector<Byte *> &vct, bool bAll);
+  Byte *Apply();
+  void Release(Byte *bys);
+
 protected:
-  unordered_map<Byte*, Buffer*> _mapBuffer;
-  unordered_map<Byte*, Buffer*> _mapFreeBuffer;
+  unordered_map<Byte *, Buffer *> _mapBuffer;
+  unordered_map<Byte *, Buffer *> _mapFreeBuffer;
   uint32_t _eleSize;
   utils::SpinMutex _spinMutex;
 };
-}
+} // namespace storage
--- src/utils/ErrorMsg.h
+++ src/utils/ErrorMsg.h
@@ -1,46 +1,39 @@
 ﻿#pragma once
 #include <exception>
+#include <mutex>
+#include <string>
 #include <unordered_map>
 #include <vector>
-#include<mutex>
-#include <string>
 
 namespace utils {
-  using namespace std;
+using namespace std;
 
-  class ErrorMsg :public exception
-  {
-  public:
-    ErrorMsg(int id, vector<string> paras = {})
-    {
-      _errId = id;
-      auto iter = _mapErrorMsg.find(id);
-      if (iter == _mapErrorMsg.end())
-      {
-        _errMsg = "Failed to find the error id, id=" + to_string(id);
-      }
-      else
-      {
-        _errMsg = iter->second;
-        for (int i = 0; i < paras.size(); i++)
-        {
-          size_t pos = _errMsg.find("{}");
-          if (pos != string::npos)
-            _errMsg.replace(pos, 2, paras[i]);
-        }
+class ErrorMsg : public exception {
+public:
+  ErrorMsg(int id, vector<string> paras = {}) {
+    _errId = id;
+    auto iter = _mapErrorMsg.find(id);
+    if (iter == _mapErrorMsg.end()) {
+      _errMsg = "Failed to find the error id, id=" + to_string(id);
+    } else {
+      _errMsg = iter->second;
+      for (int i = 0; i < paras.size(); i++) {
+        size_t pos = _errMsg.find("{}");
+        if (pos != string::npos)
+          _errMsg.replace(pos, 2, paras[i]);
       }
     }
+  }
 
-    char const* what() const override
-    {
-      return _errMsg.c_str();
-    }
-    int getErrId() { return _errId; }
-  protected:
-    static unordered_map<int, string> LoadErrorMsg();
-  protected:
-    static unordered_map<int, string> _mapErrorMsg;
-    int _errId;
-    string _errMsg;
-  };
-}
+  const char *what() const noexcept { return _errMsg.c_str(); }
+  int getErrId() { return _errId; }
+
+protected:
+  static unordered_map<int, string> LoadErrorMsg();
+
+protected:
+  static unordered_map<int, string> _mapErrorMsg;
+  int _errId;
+  string _errMsg;
+};
+} // namespace utils
