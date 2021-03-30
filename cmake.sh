953d6d8558cde431e3d87ee17da121038d8779b7
Guanjun Li <lgj@ubuntu-3.lgj>
2021-03-30 09:18:25 +0800

    add cmake.sh

---
+++ cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+if [ -d ./Release ] ; then 
+  rm -r ./Release
+fi
+
+mkdir ./Release
+cd Release
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ ..
+cd ..
+
+if [ -d ./Debug ] ; then 
+  rm -r ./Debug
+fi
+
+mkdir ./Debug
+cd Debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ .. 
+cd ..
+
