a708966069a28b343a9f407fd68f04e8ac9b5160
冠军 李 <lgj123456@163.com>
2021-07-28 11:02:15 +0800

    update BaseExpr.h

--- src/expr/BaseExpr.h
+++ src/expr/BaseExpr.h
@@ -1,11 +1,53 @@
 ﻿#pragma once
+#include "../dataType/IDataValue.h"
+#include <vector>
+
+using namespace std;
 namespace storage {
-class BaseExpr {
-public:
-protected:
+enum class ExprType {
+  EXPR_COLUMNS,
+  EXPR_WHERE,
+  EXPR_ON,
+  EXPR_VALUE,
+  EXPR_PARA,
+  EXPR_COLUMN,
+  EXPR_FUNCTION,
+  EXPR_OPERATOR,
+  EXPR_SELECT,
+  EXPR_ARRAY,
+  EXPR_CASE,
+  EXPR_ADD,
+  EXPR_SUB,
+  EXPR_MUL,
+  EXPR_DIV,
+  EXPR_AND,
+  EXPR_OR,
+  EXPR_IN,
+  EXPR_LIKE,
+  EXPR_NOT,
+  EXPR_ISNULL,
+  EXPR_EXIST,
+  EXPR_MINUS
 };
 
-class ValueExpr : BaseExpr {};
+class BaseExpr {
+public:
+  ~BaseExpr() {}
+  virtual IDataValue *op(VectorDataValue &vctDv) = 0;
+  ExprType GetType() { return _exprType; }
+
+protected:
+  ExprType _exprType;
+};
+
+class ValueExpr : BaseExpr {
+public:
+  ValueExpr(int vPos) : _valPos(vPos) {}
+  IDataValue *op(VectorDataValue &vctDv) {}
+
+protected:
+  int _valPos;
+};
 class ParameterExpr : BaseExpr {};
 
 class ColumnExpr : BaseExpr {};
