fa65775dd72feb517e2438e2c33222ea9621afed
Guanjun Li <lgj123456@163.com>
2023-08-06 20:12:39 +0800

    middle save 2023-8-6

--- src/expr/BaseExpr.h
+++ src/expr/BaseExpr.h
@@ -51,6 +51,7 @@ enum class ExprType {
   // Input or oupt value and table
   EXPR_EDIT_COLUMN,
   EXPR_TABLE,
+  EXPR_RESULT_COLUMN,
 
   // Select
   EXPR_TABLE_SELECT,
@@ -72,7 +73,6 @@ class BaseExpr {
 public:
   virtual ~BaseExpr() {}
   virtual ExprType GetType() { return ExprType::EXPR_BASE; }
-  virtual void Preprocess(){};
 
   static void *operator new(size_t size) {
     return CachePool::Apply((uint32_t)size);
@@ -136,31 +136,6 @@ public:
 };
 
 /**
- * @brief The column information for insert or update. Here will use this to
- * save the data value into row data value array
- */
-class ExprEditColumn : public BaseExpr {
-public:
-  ExprEditColumn(const string &name)
-      : _name(name), _pos(-1), _exprData(nullptr) {}
-  ~ExprEditColumn() { delete _exprData; }
-  ExprType GetType() { return ExprType::EXPR_EDIT_COLUMN; }
-  void Preprocess(int pos, ExprData *exprData) {
-    _pos = pos;
-    _exprData = exprData;
-  }
-
-  bool Calc(VectorDataValue &vdPara, VectorDataValue &vdRow) {
-    IDataValue *dv = _exprData->Calc(vdPara, vdRow);
-  }
-
-protected:
-  string _name;        // column name
-  int _pos;            // The position in table columns
-  ExprData *_exprData; // The expression to get data value
-};
-
-/**
  * @brief To save array const values, to be used in SQL operator IN.
  */
 class ExprArray : public BaseExpr {
@@ -187,190 +162,68 @@ protected:
 class ExprTable : public BaseExpr {
 public:
   ExprTable(string &dbName, string &name, string &alias)
-      : _dbName(dbName), _tableName(name), _tableAlias(alias) {
-    if (_tableAlias.size() == 0)
-      _tableAlias = _tableName;
+      : _dbName(move(dbName)), _tName(move(name)), _tAlias(alias) {
+    if (_tAlias.size() == 0)
+      _tAlias = _tName;
   }
   ~ExprTable() {}
   ExprType GetType() { return ExprType::EXPR_TABLE; }
 
   const string &GetDbName() const { return _dbName; }
-  const string &GetTableName() const { return _tableName; }
-  const string &GetTableAlias() const { return _tableAlias; }
-  void SetDbName(const string &dbName) { _dbName = dbName; }
+  const string &GetTableName() const { return _tName; }
+  const string &GetTableAlias() const { return _tAlias; }
+  void SetDbName(string &dbName) { _dbName = move(dbName); }
 
 protected:
   string _dbName;
-  string _tableName;
-  string _tableAlias;
+  string _tName;
+  string _tAlias;
+};
+
+/**
+ * @brief The column information for insert or update. Here will use this to
+ * get the data value from exist row data or parameters, then save it into row
+ * data value array
+ */
+class ExprEditColumn : public BaseExpr {
+public:
+  ExprEditColumn(string &name)
+      : _name(move(name)), _pos(-1), _exprData(nullptr) {}
+  ~ExprEditColumn() { delete _exprData; }
+  ExprType GetType() { return ExprType::EXPR_EDIT_COLUMN; }
+  void Preprocess(int pos, ExprData *exprData) {
+    _pos = pos;
+    _exprData = exprData;
+  }
+
+  bool Calc(VectorDataValue &vdPara, VectorDataValue &vdRow) {
+    IDataValue *dv = _exprData->Calc(vdPara, vdRow);
+    vdRow[_pos]->Copy(*dv, !dv->IsReuse());
+    if (!dv->IsReuse()) {
+      delete dv;
+    }
+  }
+
+protected:
+  string _name;        // column name
+  int _pos;            // The position in table columns
+  ExprData *_exprData; // The expression to get data value from source
 };
 
 // The column in temp table for select result.
 class ExprResColumn : public BaseExpr {
 public:
-  ExprResColumn(int index, string &&colName, DataType dt, ExprData *data,
-                int dataBasicStart, int prevVarCols, int colNullPlace)
-      : _index(index), _colName(move(colName)), _dataType(dt), _data(data),
-        _dataBasicStart(dataBasicStart), _prevVarCols(prevVarCols),
-        _colNullPlace(colNullPlace) {}
-  ~ExprColumn() { delete _data; }
+  ExprResColumn(string &name) : _name(move(name)) {}
+  ~ExprResColumn() {}
+  ExprType GetDataType() const { return ExprType::EXPR_RESULT_COLUMN; }
+  void Preprocess() {}
+  void Calc(VectorDataValue &vdSrc, VectorDataValue &vdDst) {}
 
-  void Calc(VectorDataValue &vdSrc, VectorDataValue &vdDst) {
-    IDataValue *pDv = _data->Calc(vdSrc, vdDst);
-    vdDst[_index]->Copy(*pDv, !pDv->IsReuse());
-    if (!pDv->IsReuse())
-      delete pDv;
-  }
-
-  ExprType GetDataType() { return ExprType::EXPR_COLUMN; }
-  int GetIndex() { return _index; }
-  const string &GetColumnName() { return _colName; }
-  const int GetDataBasicStart() const { return _dataBasicStart; }
-  const int GetPrevVarCols() const { return _prevVarCols; }
-  const int GetColNullPlace() const { return _colNullPlace; }
-
-  /** Return the column's data start position, if is null, return -1
-   * @param bys the begin address for this record.
-   * @return -1: if value is null; >= 0: the actual start position of column
-   * data
-   */
-  int CalcPosition(Byte *bys) {
-    if ((bys[sizeof(int32_t) + _index / BYTE_SIZE] &
-         (Byte)(1 << (_index % BYTE_SIZE))) == 0) {
-      return -1;
-    } else {
-      return _dataBasicStart +
-             (_prevVarCols <= 0
-                  ? 0
-                  : UInt32FromBytes(bys + _colNullPlace +
-                                    _prevVarCols * sizeof(int32_t)));
-    }
-  }
-  /**To judge if a column'svalue in a row is null*/
-  bool IsNull(Byte *bys) {
-    return bys[_index / BYTE_SIZE] & (1 << (_index % BYTE_SIZE));
-  }
-
-  /**
-   * Return the data length for this column
-   * @param bys the row data begin bytes.
-   * @return the actual data length, if null, return 0;
-   */
-  int GetLength(Byte *bys) {
-    if ((bys[UI32_LEN + _index / BYTE_SIZE] &
-         (Byte)(1 << (_index % BYTE_SIZE))) == 0) {
-      return 0;
-    }
-
-    switch (_dataType) {
-    case DataType::LONG:
-    case DataType::ULONG:
-    case DataType::DOUBLE:
-    case DataType::DATETIME:
-      return 8;
-    case DataType::FLOAT:
-    case DataType::INT:
-    case DataType::UINT:
-      return 4;
-    case DataType::SHORT:
-    case DataType::USHORT:
-      return 2;
-    case DataType::BOOL:
-    case DataType::CHAR:
-    case DataType::BYTE:
-      return 1;
-    case DataType::FIXCHAR:
-    case DataType::VARCHAR:
-    case DataType::BLOB: {
-      int pos = sizeof(int32_t) + _colNullPlace;
-      return (_prevVarCols == 0
-                  ? UInt32FromBytes(bys + pos)
-                  : UInt32FromBytes(bys + pos + _prevVarCols * UI32_LEN) -
-                        UInt32FromBytes(bys + pos + _prevVarCols * UI32_LEN -
-                                        UI32_LEN));
-    }
-    default:
-      return -1;
-    }
-  }
-  /**
-   * compare the same column for two records
-   * @param bys1
-   * @param bys2
-   * @return
-   */
-  int CompareTo(Byte *bys1, Byte *bys2) {
-    int pos1 = CalcPosition(bys1);
-    int pos2 = CalcPosition(bys2);
-    if (pos1 < 0 && pos2 < 0) {
-      return 0;
-    } else if (pos1 < 0) {
-      return -1;
-    } else if (pos2 < 0) {
-      return 1;
-    }
-
-    Byte *b1 = bys1 + pos1;
-    Byte *b2 = bys2 + pos2;
-    switch (_dataType) {
-    case DataType::LONG:
-      return (int)(*((int64_t *)b1) - *((int64_t *)b2));
-    case DataType::ULONG:
-      return (int)(*((uint64_t *)b1) - *((uint64_t *)b2));
-    case DataType::DOUBLE:
-      return (int)(*((double *)b1) - *((double *)b2));
-    case DataType::DATETIME:
-      return (int)(*((uint64_t *)b1) - *((uint64_t *)b2));
-    case DataType::FLOAT:
-      return (int)(*((float *)b1) - *((float *)b2));
-    case DataType::INT:
-      return *((int32_t *)b1) - *((int32_t *)b2);
-    case DataType::UINT:
-      return *((uint32_t *)b1) - *((uint32_t *)b2);
-    case DataType::SHORT:
-      return *((int16_t *)b1) - *((int16_t *)b2);
-    case DataType::USHORT:
-      return *((uint16_t *)b1) - *((uint16_t *)b2);
-    case DataType::BOOL:
-      return (*b1 ? 1 : 0) - (*b2 ? 1 : 0);
-    case DataType::CHAR:
-      return *((char *)b1) - *((char *)b2);
-    case DataType::BYTE:
-      return *b1 - *b2;
-    case DataType::FIXCHAR:
-    case DataType::VARCHAR:
-    case DataType::BLOB:
-      return BytesCompare(b1, GetLength(bys1), b2, GetLength(bys2));
-    default:
-      throw new ErrorMsg(DT_UNKNOWN_TYPE, {to_string((uint32_t)_dataType)});
-    }
-  }
+  const string &GetColumnName() const { return _name; }
 
 protected:
-  // The index in table's columns array
-  int _index;
-  // Column name
-  string _colName;
-  // The related data type for this column
-  DataType _dataType;
-  // For no const type, to tell how to get parameter from array.
-  ExprData *_data;
-  // Below 3 variable used to fast pick up a field in records.
-  // The structure for a record data:
-  // 1. n bytes to save if the columns' values are null, 8 columns per byte
-  // 2. 2 bytes * n variable columns, to save total length of variable
-  columns
-      // before and include this column
-      // 3. the actual value for every columns, if value is null, not occupy
-      bytes.
-      /** The data start position in a record value byte array.
-       * This value does not include the variable columns' length.
-       */
-      int _dataBasicStart;
-  /**To save how many variable columns(String or blob) before this column*/
-  int _prevVarCols;
-  /**How many bytes to save if the columns are null, 8 columns per byte*/
-  int _colNullPlace;
+  string _name;        // Column name
+  int _pos;            // The index in table's columns array
+  ExprData *_exprData; // The expression to get data value from source
 };
-
 } // namespace storage
