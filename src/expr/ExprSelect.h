f54792698e11ae0666bf2ee1207767e7f92d7b8c
Guanjun Li <lgj123456@163.com>
2023-08-17 21:41:16 +0800

    middle save 2023-8-17

--- src/expr/ExprSelect.h
+++ src/expr/ExprSelect.h
@@ -16,17 +16,6 @@
 using namespace std;
 namespace storage {
 
-// To query physical table, point out which index will be used. Only one index
-// can be selected.
-struct SelIndex {
-  // If the primary or secondary index can be used to query, copy the query
-  // conditions to here. Only one index can be used. Only valid for physical
-  // table select
-  ExprLogic *_indexExpr;
-  // which index used, The position of index that start from 0(primary key)
-  int _indexPos;
-};
-
 enum class TableType {
   PersistTable, // From physical table
   CacheTable,   // The result saved in temp block table
